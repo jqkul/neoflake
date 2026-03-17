8ee79adb64eb02219a568c97b7c9052ff5d5d49b
Jacqueline Kulas <jqkulas@gmail.com>
2026-03-16 23:03:15 -0400

    fix: make setup_macro integration test actually run

--- tests/setup_macro.rs
+++ tests/setup_macro.rs
@@ -4,11 +4,12 @@ const MY_EPOCH: u64 = 1767225600000;
 
 global_snowflake::setup!(Snowflake, MY_EPOCH, SNOWFLAKE_GEN, 0);
 
-fn main() {
+#[test]
+fn setup_uses_correct_epoch() {
     let epoch_dt = DateTime::from_timestamp_millis(MY_EPOCH.try_into().unwrap()).unwrap();
     let expected_difference = Utc::now() - epoch_dt;
     for _ in 0..10 {
-        let flake = SNOWFLAKE_GEN.generate();
+        let flake: Snowflake = SNOWFLAKE_GEN.generate();
         let ts = flake.timestamp_millis_unix();
         let dt = DateTime::from_timestamp_millis(ts.try_into().unwrap()).unwrap();
 
